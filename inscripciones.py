b10682852e0683f4d09be9a0f171d376aa1ddd26
martini-botiniol <mbotina@unal.edu.co>
2024-05-20 20:44:21 -0500

    cambio visual en boton consultar

--- inscripciones.py
+++ inscripciones.py
@@ -1473,14 +1473,19 @@ class Inscripciones_2:
 
         self.frame_treeview = tk.Frame(ventanaConsulta)
         self.frame_treeview.pack(padx=10, pady=10, fill="both", expand=True)
-        self.treeview_consulta = ttk.Treeview(self.frame_treeview, columns=("columna1","columna2"), show="headings")
-        self.treeview_consulta.heading("columna1", text="Datos de la Consulta")
+        self.treeview_consulta = ttk.Treeview(self.frame_treeview, columns=("columna1", "columna2"), show="headings")
+        self.treeview_consulta.heading("columna1", text="")
+        self.treeview_consulta.heading("columna2", text="")
+        self.treeview_consulta.column("columna1", width=120)
+        self.treeview_consulta.column("columna2", width=150)
         self.treeview_consulta.pack(side="left", fill="both", expand=True)
+
         self.combo_estudiantes.bind("<<ComboboxSelected>>", self.consultar_cursos_del_estudiante)
         self.combo_cursos.bind("<<ComboboxSelected>>", self.consultar_estudiantes_del_curso)
 
     def consultar_cursos_del_estudiante(self, event):
         self.treeview_consulta.delete(*self.treeview_consulta.get_children())  
+        self.treeview_consulta.heading("columna1", text="Descripción del Curso")
         self.cursor = self.connection.cursor()
         id_Alumno = self.combo_estudiantes.get()
         self.cursor.execute("SELECT Código_Curso FROM Inscritos WHERE Id_Alumno = ?", (id_Alumno,))
@@ -1496,6 +1501,7 @@ class Inscripciones_2:
 
     def consultar_estudiantes_del_curso(self, event):
         self.treeview_consulta.delete(*self.treeview_consulta.get_children()) 
+        self.treeview_consulta.heading("columna1", text="Nombres Completos")
         self.cursor = self.connection.cursor()
         id_Curso = self.combo_cursos.get()
         self.cursor.execute("SELECT Código_Curso FROM Cursos WHERE Descripción_Curso = ?", (id_Curso,))
@@ -1510,8 +1516,6 @@ class Inscripciones_2:
             alumnos = self.cursor.fetchone()
             self.treeview_consulta.insert("", "end", values=alumnos)
             self.cursor.close()
-            print(alumnos)
-
 
 if __name__ == "__main__":
     app = Inscripciones_2()
